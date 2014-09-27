876b681898cfcbee615176d8b138bc4eaf9e2cde
Brian Sorahan <bsorahan@gmail.com>
2014-09-27 15:49:08 -0500

    fixing build

--- install-deps.sh
+++ install-deps.sh
@@ -3,11 +3,8 @@
 function main {
     git clone https://github.com/lightning/liblightning.git
     cd liblightning
-    ./install-opus.sh
-    ./install-check.sh
     make
     sudo make install
-    cd .. && ./install-pkg.sh
 }
 
 main "$@"
