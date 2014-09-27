a99b5acda2f5eb30c3a6f828d424ea2a80729273
Brian Sorahan <bsorahan@gmail.com>
2014-09-27 17:59:19 -0500

    fix example

--- example/play-sample.go
+++ example/play-sample.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"github.com/lightning/go/binding"
-	"github.com/lightning/go/seq"
+	"github.com/lightning/go"
 	"time"
 )
 
 func main() {
-	engine := binding.NewEngine()
+	engine := lightning.NewEngine()
 	engine.Connect("system:playback_1", "system:playback_2")
-	note := seq.NewNote("/home/brian/lightning/kits/meow.wav", 60, 72)
+	note := lightning.NewNote("/home/brian/lightning/kits/meow.wav", 60, 72)
 	engine.PlayNote(note)
 	// engine.PlaySample("/home/brian/lightning/kits/meow.wav", 1.0, 1.0)
 	time.Sleep(3000 * time.Millisecond)
